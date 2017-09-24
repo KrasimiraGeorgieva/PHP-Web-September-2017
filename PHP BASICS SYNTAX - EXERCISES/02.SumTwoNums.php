5949d7cb9b75a4ea0e48f96ae51908a03a3405ee
Krasimira Georgieva <krassi77bg@gmail.com>
2017-09-25 01:24:12 +0300

    Added 02.SumTwoNumbers solution.

---
+++ PHP BASICS SYNTAX - EXERCISES/02.SumTwoNums.php
@@ -0,0 +1,6 @@
+<?php
+$firstNumber = 1234.5678;
+$secondNumber = 333;
+$result = sprintf('%0.2f', $firstNumber + $secondNumber);
+
+echo '$firsNumber + $secondNumber = ' . "$firstNumber + $secondNumber" . ' = ' . $result;
