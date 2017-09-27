cfe6a1ddcb743b8636024e6e692fc5fd4b43797b
Krasimira Georgieva <krassi77bg@gmail.com>
2017-09-28 01:23:36 +0300

    Initial 06.Mixed Phones solution.

---
+++ PHP ADVANCED SYNTAX - EXERCISES/06.MixedPhones.php
@@ -0,0 +1,18 @@
+<?php
+while(1){
+    $input = trim(fgets(STDIN));
+    if ($input == 'Over'){
+        break;
+    }
+    $data = explode(' : ', $input);
+    if (preg_match('/^[0-9]{1,10}$/', $data[0])){
+        $temp = $data[0];
+        $data[0] = $data[1];
+        $data[1] = $temp;
+    }
+    $results[$data[0]] = $data[1];
+}
+ksort($results);
+foreach ($results as $name=>$phone){
+    echo $name.' -> '.$phone."\r\n";
+}
