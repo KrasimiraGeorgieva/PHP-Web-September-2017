743085fbfc7926bf7d44195493090f9a9ce5e68d
Krasimira Georgieva <krassi77bg@gmail.com>
2017-10-07 23:23:12 +0300

    Added task solution.

---
+++ OOP FUNDAMENTALS PART I-LAB/07.ProtectingTheNumberOfDoorsSetter/Vehicle_7.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+class Vehicle_7
+{
+    protected $color;
+    private $numberDoors;
+
+    public function  __construct(string $color, string $numberDoors){
+        $this->color = $color;
+        $this->numberDoors = $numberDoors;
+    }
+
+    public function getNumberDoors(): string
+    {
+        return $this->numberDoors;
+    }
+
+    protected function setNumberDoors(string $numberDoors)
+    {
+        $this->numberDoors = $numberDoors;
+    }
+
+    public function __get($name){
+        if($this->{$name}) {
+            return $this->{$name};
+        }
+        else{
+            return "Property does not exist!";
+        }
+    }
+}
