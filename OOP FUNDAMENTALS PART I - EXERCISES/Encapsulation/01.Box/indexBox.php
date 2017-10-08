e4b37d6072cdb4ddcda5af8821d87a71421531e3
Krasimira Georgieva <krassi77bg@gmail.com>
2017-10-08 23:56:37 +0300

    Added task solution.

---
+++ OOP FUNDAMENTALS PART I - EXERCISES/Encapsulation/01.Box/indexBox.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+require_once 'ClassBox.php';
+
+$input = [];
+for ($i = 0; $i<3; $i++) {
+    $input[] = floatval(fgets(STDIN));
+}
+$box = new Box($input[0], $input[1], $input[2]);
+echo $box;
