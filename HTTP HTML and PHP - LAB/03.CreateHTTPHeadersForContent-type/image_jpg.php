5e56baea028a76c263a2895db68466366bc2743b
Krasimira Georgieva <krassi77bg@gmail.com>
2017-10-28 00:02:36 +0300

    Added solution.

---
+++ HTTP HTML and PHP - LAB/03.CreateHTTPHeadersForContent-type/image_jpg.php
@@ -0,0 +1,10 @@
+<?php
+header("Content-Type: image/jpg");
+
+/*
+ HTTP/1.1 200 OK
+X-Powered-By: PHP/7.1.7
+Content-Type: image/jpg
+server: PhpStorm 2017.2.4
+content-length: 0
+ */
---
+++ HTTP HTML and PHP - LAB/03.CreateHTTPHeadersForContent-type/text_html.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/html");
+
+//Response Header
+//HTTP/1.1 200 OK
+//X-Powered-By: PHP/7.1.7
+//Content-type: text/html;charset=UTF-8
+//server: PhpStorm 2017.2.4
+//content-length: 0
