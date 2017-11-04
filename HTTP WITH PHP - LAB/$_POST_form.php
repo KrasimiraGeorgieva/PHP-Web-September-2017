8af7a7eb4c6ec6cbc8adc735c401eec48a43dce5
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-04 15:55:19 +0200

    Initial commit.

---
+++ HTTP WITH PHP - LAB/$_POST_form.php
@@ -0,0 +1,9 @@
+<form action="register.php" method="post">
+    Username: <input type="text" name="username" /><br>
+    Password: <input type="text" name="password" /><br>
+
+    <input type="submit" name="Register" /><br>
+</form>
+<?php
+
+var_dump($_POST);
