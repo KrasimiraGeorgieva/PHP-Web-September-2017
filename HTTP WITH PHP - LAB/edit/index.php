21114febfd18e1f6731098fdbe4a606b9c57acd4
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-04 15:55:35 +0200

    Initial commit.

---
+++ HTTP WITH PHP - LAB/edit/index.php
@@ -0,0 +1,11 @@
+<form action="edit.php?id=1" method="post">
+    Username: <input type="text" name="username" /><br>
+    Password: <input type="password" name="password" /><br>
+    <input type="submit" name="Edit" /><br>
+</form>
+<hr>
+<form action="edit.php?id=2" method="post">
+    Username: <input type="text" name="username" /><br>
+    Password: <input type="password" name="password" /><br>
+    <input type="submit" name="Edit" /><br>
+</form>
