ed8bf9adfb80c2d9f7df9bb84abecac51a6e7615
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-11 23:23:54 +0200

    Initial commit.

---
+++ Exam Preparation/Database/PDOResultSet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database;
+
+
+class PDOResultSet implements ResultSetInterface
+{
+    /**
+     * @var \PDOStatement
+     */
+    private $pdoStatement;
+
+    public function __construct(\PDOStatement $pdoStatement)
+    {
+        $this->pdoStatement = $pdoStatement;
+    }
+
+    public function fetch($className): \Generator
+    {
+        while($row = $this->pdoStatement->fetchObject($className)){
+            yield $row; //yield-връща всени път, когато функцията се извика по един следващ елемент в while-цикъла
+        }
+    }
+
+    public function fetchColumn(int $colNum = 0)
+    {
+        // TODO: Implement fetchColumn() method.
+    }
+}
