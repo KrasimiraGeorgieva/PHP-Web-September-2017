22a593f3c6ede89bccd6d73f6dd31d5ddf5c245d
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-11 23:24:41 +0200

    Initial commit.

---
+++ Exam Preparation/loginPage.php
@@ -0,0 +1,12 @@
+<form method="post">
+<label>
+    Username:
+    <input type="text" name="username" required/><br>
+</label>
+<label>
+    Password:
+    <input type="password" name="password" required/><br>
+</label>
+
+<input type="submit" name="login" value="Login!"/>
+</form>
