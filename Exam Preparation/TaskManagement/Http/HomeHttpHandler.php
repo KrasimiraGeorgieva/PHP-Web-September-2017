4bd412c2ddb1a7f0b9ca53741b7d81c049cebb7b
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-12 23:44:06 +0200

    Initial commit.

---
+++ Exam Preparation/TaskManagement/Http/HomeHttpHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krasimira
+ * Date: 11/10/2017
+ * Time: 20:52
+ */
+namespace TaskManagement\HomeHttpHandler;
+class HomeHttpHandler
+{
+    public function index()
+    {
+
+    }
+}
