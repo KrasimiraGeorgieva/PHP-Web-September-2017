d7895afacfbfbfd29cd192b550f8d90951ea4aac
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-14 21:31:29 +0200

    Initial commit.

---
+++ ExamPHPWeb-12-11-2017/MyMoney/Templates/operations/operations.php
@@ -0,0 +1,19 @@
+<h1>All Operations</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Type</th>
+        <th>Reason</th>
+        <th>Sum</th>
+        <th>Notes</th>
+        <th>Edit</th>
+        <th>Delete</th>
+    </tr>
+    </thead>
+</table>
+<ul>
+    <li><a href="add_operation.php">Add new operation</a></li>
+    <li><a href="statistics.php">Statistics</a></li>
+    <li><a href="../../../logout.php">Logout</a></li>
+</ul>
