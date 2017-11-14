109545bea5df6a90dd38972b32155be46b086797
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-14 21:31:13 +0200

    Initial commit.

---
+++ ExamPHPWeb-12-11-2017/MyMoney/Templates/operations/add_operation.php
@@ -0,0 +1,57 @@
+<h1>All Operations</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Type</th>
+        <th>Reason</th>
+        <th>Sum</th>
+        <th>Notes</th>
+        <th>Edit</th>
+        <th>Delete</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>13/11/2017</td>
+        <td>Expense</td>
+        <td>Coffee</td>
+        <td>2,50</td>
+        <td>Costa</td>
+        <td><a href="edit_task.php?id=2">edit</a></td>
+        <td><a href="delete_task.php?id=2">delete</a></td>
+    </tr>
+    <tr>
+        <td>15/11/2017</td>
+        <td>Income</td>
+        <td>Salary</td>
+        <td>500</td>
+        <td></td>
+        <td><a href="edit_task.php?id=4">edit</a></td>
+        <td><a href="delete_task.php?id=4">delete</a></td>
+    </tr>
+    <tr>
+        <td>10/11/2017</td>
+        <td>Expense</td>
+        <td>Party</td>
+        <td>120,09</td>
+        <td>Club</td>
+        <td><a href="edit_task.php?id=5">edit</a></td>
+        <td><a href="delete_task.php?id=5">delete</a></td>
+    </tr>
+    <tr>
+        <td>6/11/2017</td>
+        <td>Income</td>
+        <td>Loan</td>
+        <td>66,67</td>
+        <td>Neighbour</td>
+        <td><a href="edit_task.php?id=3">edit</a></td>
+        <td><a href="delete_task.php?id=3">delete</a></td>
+    </tr>
+    </tbody>
+</table>
+<ul>
+    <li><a href="add_operation.php">Add new operation</a></li>
+    <li><a href="statistics.php">Statistics</a></li>
+    <li><a href="../../../logout.php">Logout</a></li>
+</ul>
