f2a424b3eb4c45a1776edb157c9ae2c0dafc553a
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-20 00:01:44 +0200

    Added solution.

---
+++ Exam Preparation/TaskManagement/Data/EditTaskDTO.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace TaskManagement\Data;
+
+
+class EditTaskDTO
+{
+    /**
+     * @var TaskDTO
+     */
+    private $task;
+
+    /**
+     * @var CategoryDTO[]
+     */
+    private $categories;
+
+    /**
+     * @return TaskDTO
+     */
+    public function getTask(): TaskDTO
+    {
+        return $this->task;
+    }
+
+    /**
+     * @param TaskDTO $task
+     */
+    public function setTask(TaskDTO $task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * @return CategoryDTO[]
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+    }
+}
